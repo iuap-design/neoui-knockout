7aed0b01f0e98c65f3950fa34507c3cafb99f1c9
Boyuzhou <2470200492@qq.com>
2016-09-13 19:10:41 +0800

    add test

---
+++ test/likeWidget.spec.js
@@ -0,0 +1,22 @@
+var expect = require('chai').expect;
+var ko = require('knockout');
+
+describe('like-widget test', function () {
+    function Product(name, rating) {
+        this.name = name;
+        this.userRating = ko.observable(rating || null);
+    }
+
+    function MyViewModel() {
+        this.products = [
+            new Product('Garlic bread'),
+            new Product('Pain au chocolat'),
+            new Product('Seagull spaghetti', 'like') // This one was already 'liked'
+        ];
+    }
+
+    ko.applyBindings(new MyViewModel());
+    describe('like-widget should be exsit', function () {
+
+    });
+});
